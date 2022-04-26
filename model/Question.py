19353f9d856f5099b7932bd86518ec75f8248a2e
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-04-26 16:18:20 -0500

    Agregando comentarios de documentacion a la clase Questions

--- model/Question.py
+++ model/Question.py
@@ -1,5 +1,9 @@
+# Clase esta creada para poder gestionar manera clara, las preguntas
+# Y como se podran enviar
 class Question:
     __preguntas = [];
 
+    # Metodo encargado de buscar la pregunta, recibiendo un numero 
+    # Para poder compararlo
     def getPregunta(self, numero):
         return self.__preguntas[numero];
