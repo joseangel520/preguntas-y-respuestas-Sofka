0e3e4ed98d434b9b94ad0a54a54e5bd934d68458
joseangel520 <jose.alvarezbe@cecar.edu.co>
2022-04-26 17:26:54 -0500

    agregando dos preguntas en Question

--- model/Question.py
+++ model/Question.py
@@ -1,18 +1,15 @@
+
 # Clase esta creada para poder gestionar manera clara, las preguntas
 # Y como se podran enviar
 class Question:
     __preguntas = [
-         []
+         ["Cual de las siguientes opciones corresponden a tipos de Sistemas Operativos Existentes en el mercado."],
+         ["¿Que programa de ofimática utilizamos para escribir cartas?"]
     ];
 
-     
 
     # Metodo encargado de buscar la pregunta, recibiendo un numero 
     # Para poder compararlo
     def getPregunta(self, numero):
-<<<<<<< HEAD
         return self.__preguntas[numero];
         
-=======
-        return self.__preguntas[numero]
->>>>>>> 78113b800e9a54748d38d32cb22954827113e253
