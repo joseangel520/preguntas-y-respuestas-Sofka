f6b2058a57e0a5c1571ce4ab2e8e55a1225340e7
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-04-27 23:58:48 -0500

    Agregando las funcionalidades de los botones del juego

--- view/View.py
+++ view/View.py
@@ -8,13 +8,12 @@ class View(ttk.Frame):
 
         super().__init__(parent)
 
-        self.title = ('Datos del Usuario')
+        self.title = ('Preguntas y Respuestas')
         self.answer = tk.StringVar()
 
         # Creando widgets
         # Label donde se mostrara la preguntas
         self.preguntas = ttk.Label(self, text='Pregunta')
-        self.preguntas.grid(row=1, column=0)
 
         # Radio buttons respuestas
         self.radioA = ttk.Radiobutton(self, text='A', value='A', variable=self.answer, command=self.selected)
@@ -22,11 +21,45 @@ class View(ttk.Frame):
         self.radioC = ttk.Radiobutton(self, text='C', value='C', variable=self.answer, command=self.selected)
         self.radioD = ttk.Radiobutton(self, text='D', value='D', variable=self.answer, command=self.selected)
 
-        self.radioA.grid(row=2, column=0)
-        self.radioB.grid(row=3, column=0)
-        self.radioC.grid(row=4, column=0)
-        self.radioD.grid(row=5, column=0)
 
+        # Buttones para iniciar el Juego, Terminar, Salir y Siguiente
+        self.btnStarGame = ttk.Button(self, text="Iniciar Juego")
+        self.btnStarGame['command'] = self.startGame
+        self.btnEndGame = ttk.Button(self, text="Terminar Juego")
+        self.btnEndGame['command'] = self.endGame
+        self.btnExit = ttk.Button(self, text="Salir Juego")
+        self.btnExit['command'] = self.exitGame
+        self.btnNext = ttk.Button(self, text = "Siguente Pregunta")
+        self.btnNext['command'] = self.next
+        
+        # Agregando los widgets a la ventana
+        # Label preguntas
+        self.preguntas.grid(row = 1, column = 0)
+        # Radio buttons respuestas
+        self.radioB.grid(row = 3, column = 0)
+        self.radioC.grid(row = 4, column = 0)
+        self.radioA.grid(row = 2, column = 0)
+        self.radioD.grid(row = 5, column = 0)
+        # Bottons funcionalidades de inicio, terminar, salir y siguiente
+        self.btnStarGame.grid(row = 2, column = 2)
+        self.btnEndGame.grid(row = 3, column = 2)
+        self.btnExit.grid(row = 4, column = 2)
+        self.btnNext.grid(row = 5, column = 2)
+
+
+    def startGame(self):
+        print("Inicio el juego")
+
+    def endGame(self):
+        print("Termino el Juego")
+    
+    def exitGame(self):
+        print("Salir el Juego")
+
+    def next(self):
+        print("Siguiente Pregunta")
+
+    # Metodo encargado de verificar la opcion que selecciono el usuario
     def selected(self):
         if self.answer.get() == "A":
             print("Selecciono A")
