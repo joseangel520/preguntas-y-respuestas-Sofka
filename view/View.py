690493b969e0bf2344d37cf1ac3715e2d5d4f2ea
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-04-27 23:42:11 -0500

    Agregando los radio botones para que el usuario pueda seleccionar la respuesta correcta

--- view/View.py
+++ view/View.py
@@ -1,24 +1,38 @@
 from tkinter import ttk
+import tkinter as tk
+
 
 class View(ttk.Frame):
+
     def __init__(self, parent):
+
         super().__init__(parent)
-        
+
         self.title = ('Datos del Usuario')
-        
+        self.answer = tk.StringVar()
+
         # Creando widgets
-        # Label Informacion Jugador
-        self.playerName = ttk.Label(self, text="Nombre Jugador: ")
-        self.surnamePlayer = ttk.Label(self, text="Apellido Jugador: ")
-        self.emailPlayer = ttk.Label(self, text="Email Jugador: ")
-        
-        
-        
-        self.playerName.grid(row=1, column=0)
-        self.surnamePlayer.grid(row=2, column=0)
-        self.emailPlayer.grid(row=3, column=0)
+        # Label donde se mostrara la preguntas
+        self.preguntas = ttk.Label(self, text='Pregunta')
+        self.preguntas.grid(row=1, column=0)
 
+        # Radio buttons respuestas
+        self.radioA = ttk.Radiobutton(self, text='A', value='A', variable=self.answer, command=self.selected)
+        self.radioB = ttk.Radiobutton(self, text='B', value='B', variable=self.answer, command=self.selected)
+        self.radioC = ttk.Radiobutton(self, text='C', value='C', variable=self.answer, command=self.selected)
+        self.radioD = ttk.Radiobutton(self, text='D', value='D', variable=self.answer, command=self.selected)
 
+        self.radioA.grid(row=2, column=0)
+        self.radioB.grid(row=3, column=0)
+        self.radioC.grid(row=4, column=0)
+        self.radioD.grid(row=5, column=0)
 
-
-
+    def selected(self):
+        if self.answer.get() == "A":
+            print("Selecciono A")
+        elif self.answer.get() == 'B':
+            print("Selecciono B")
+        elif self.answer.get() == 'C':
+            print("Selecciono C")
+        else:
+            print("Selecciono D")
